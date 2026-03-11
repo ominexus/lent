3d1fb25b6223c804282792e012660a2ae43587eb
ominexus <34224470+ominexus@users.noreply.github.com>
2026-03-11 10:18:41 +0900

    Add files via upload

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://rbckheornjrhnvjybhff.supabase.co';
+const SUPABASE_URL      = 'https://rbckheornjrhnvjybhff.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiY2toZW9ybmpyaG52anliaGZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4Njc3NDQsImV4cCI6MjA4ODQ0Mzc0NH0.dlN5jl-7nhl00sicKSboR3-1aDMP5A8ZZCoPmT0habY';
 
 const { createClient } = supabase;
